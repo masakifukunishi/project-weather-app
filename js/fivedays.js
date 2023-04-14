85c4e597e560705d9ecdad0be02190914e07dd32
sujin323 <56923051+sujin323@users.noreply.github.com>
2023-04-14 12:21:41 -0700

    Update fivedays.js

--- js/fivedays.js
+++ js/fivedays.js
@@ -7,8 +7,8 @@ async function fiveDaysWeather(lat, lng) {
         const interval = Math.floor(data.list.length / 5);
         let count = 0;
         for (i = 0; i < data.list.length; i += interval) {
-          document.getElementById(`day${count + 1}Min`).innerHTML = "Min: " + Math.round(data.list[i].main.temp_min) + " ℃";
-          document.getElementById(`day${count + 1}Max`).innerHTML = "Max: " + Math.round(data.list[i].main.temp_max) + " ℃";
+          document.getElementById(`day${count + 1}Min`).innerHTML = "Min:" + Math.round(data.list[i].main.temp_min) + "℃";
+          document.getElementById(`day${count + 1}Max`).innerHTML = "Max:" + Math.round(data.list[i].main.temp_max) + "℃";
           document.getElementById(`img${count + 1}`).src = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
           document.querySelector(`.weather_day${count+1}`).innerText = data.list[i].weather[0].main; 
           count++;
